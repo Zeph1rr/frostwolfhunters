4d59bbf00d9f623d9a59150cdf86a2f4330b03ab
zeph1rr <grianton535@gmail.com>
2024-11-27 01:31:26 +0300

    Ready chasing MeleeEnemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,6 +5,7 @@ public abstract class Enemy : MonoBehaviour
 {
     public event EventHandler OnTakeHit;
     public event EventHandler OnDeath;
+    public event EventHandler OnAttack;
     public bool IsBoss => stats.Stats.IsBoss;
     public int ThreatLevel => stats.Stats.ThreatLevel;
     
@@ -17,7 +18,11 @@ public abstract class Enemy : MonoBehaviour
 
  //   public Resource DropType => dropType;
 
-   // protected abstract void Attack();
+   protected virtual void Update() {
+        if (attackCooldownTimer > 0) {
+            attackCooldownTimer = Math.Max(attackCooldownTimer - Time.deltaTime, 0);
+        }
+   }
    // protected abstract void Move();
     public void TakeDamage(int damage) {
         stats.Stats.CurrentHealth -= damage;
@@ -28,6 +33,14 @@ public abstract class Enemy : MonoBehaviour
     protected void DetectDeath() {
         if (stats.Stats.CurrentHealth <= 0) {
             OnDeath?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    protected void Attack() {
+        if (attackCooldownTimer == 0) {
+            Debug.Log("Attack!");
+            attackCooldownTimer += stats.Stats.AttackSpeed;
+            OnAttack?.Invoke(this, EventArgs.Empty);
         }
     }
   //  protected abstract void Die();
--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -1,42 +1,86 @@
-
+using System.Data.Common;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MeleeEnemy : Enemy
 {
-    private State _currentState;
+    [SerializeField] Transform _target;
+    [SerializeField] private State _currentState = State.Idle;
+    private Rigidbody2D _rigidBody;
 
-    public enum State {
+    private enum State {
         Idle,
         Chasing,
         Attacking,
         Dead
     }
 
+    private void Awake() {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
     private void Start() {
-        _currentState = State.Idle;
         stats.Stats.CurrentHealth = stats.Stats.MaxHealth;
         OnTakeHit += HandleOnTakaHit;
         OnDeath += HandleOnDeath;
+        if (_target == null) {
+             GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null) {
+                _target = player.transform;
+            }
+             
+        }
     }
 
-    private void Update() {
+    private void OnDestroy() {
+        OnTakeHit -= HandleOnTakaHit;
+        OnDeath -= HandleOnDeath;
+    }
+
+    protected override void Update() {
+        base.Update();
         StateHandler();
     }
 
     private void StateHandler() {
+        ChangeStateByDistance();
         switch (_currentState) {
+            case State.Chasing:
+                Chase();
+                break;
+            case State.Attacking:
+                Attack();
+                break;
             default:
             case State.Idle:
                 break;
         }
     }
 
+    private void ChangeStateByDistance() {
+        float distanceToTarget = Vector2.Distance(_target.position, transform.position);
+        if (_currentState != State.Dead) {
+            if (distanceToTarget > stats.Stats.AttackRange) {
+                _currentState = State.Chasing;
+            } else {
+                _currentState = State.Attacking;
+            }
+        }
+        
+    }
+    private void Chase() {
+        Vector2 direction = (_target.position - transform.position).normalized;
+        _rigidBody.MovePosition(_rigidBody.position + direction * (stats.Stats.Speed * Time.deltaTime));
+    }
+
     private void HandleOnTakaHit(object sender, System.EventArgs e) {
-        Debug.Log("Took hit. Current healt: " + stats.Stats.CurrentHealth);
+        Debug.Log("Took hit. Current health: " + stats.Stats.CurrentHealth);
     }
 
     private void HandleOnDeath(object sender, System.EventArgs e) {
         Debug.Log("Died");
         Destroy(gameObject);
     }
+
+
 }
