27549b0821d45ceaf014c14a8ef2685de8e693b0
zeph1rr <grianton535@gmail.com>
2025-02-25 18:03:29 +0300

    added current/max wave text and translations

--- Assets/FrostWolfHunters/Scripts/UI/UI.cs
+++ Assets/FrostWolfHunters/Scripts/UI/UI.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,18 +11,29 @@ public class UI : MonoBehaviour
 
     [SerializeField] private GameObject _pauseMenu;
     [SerializeField] private Image _cooldownImage; 
+    [SerializeField] private TextMeshProUGUI _currentWaveText;
+    [SerializeField] private TextMeshProUGUI _maxWaveText;
     
     private GameInput _gameInput;
+    private GameData _gameData;
     private float _attackCooldown;
     private float _attackSpeed;
     private Player _player;
 
-    public void Initialize(GameInput gameInput, Player player)
+    public void Initialize(GameInput gameInput, Player player, GameData gameData)
     {
         _gameInput = gameInput;
         _gameInput.OnPausePressed += HandlePause;
         _player = player;
         _player.OnPlayerAttack += HandleAttack;
+        _gameData = gameData;
+        SetText();
+    }
+
+    private void SetText()
+    {
+        _currentWaveText.text = LocalizationSystem.Translate("current_wave") + ": " + _gameData.CurrentWaveNumber;
+        _maxWaveText.text = LocalizationSystem.Translate("max_wave") + ": " + _gameData.CurrentWaveNumber;
     }
 
     private void OnDestroy()
